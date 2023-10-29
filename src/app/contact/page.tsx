10d25df85b9171ca427367a13eb5d99ebd1835ae
mbuimbogo <mbuimbogo99@gmail.com>
2023-10-29 13:47:20 +0300

    create the contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Contact from '@/components/Contact'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className='mt-32'>
+        <Contact />
+    </div>
+  )
+}
+
+export default page
