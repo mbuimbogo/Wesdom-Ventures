97a52435c04308c197e6d2dd27bb7f0a3462b462
mbuimbogo <mbuimbogo99@gmail.com>
2023-10-29 13:49:07 +0300

    List all properties and render on the ui

---
+++ src/components/ListingCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {};
+
+const ListingCard = ({ listingId, location, description, image }) => {
+  return (
+    <div className="w-full">
+      <Link href={`/listings/${listingId}`}>
+        <div className="w-[90%] mx-auto flex-col justify-center items-center bg-white  text-blue-300  gap-2 rounded-md py-2 px-2 box-border">
+          <Image
+            src={image}
+            alt="land sale "
+            className=" rounded-md  py-2 w-full h-full"
+            width={300}
+            height={300}
+          />
+          <h5 className="text-center text-[1.5rem] font-[700]">{location}</h5>
+          <p className="text-center mb-4 text-[1.3rem] font-[400]">
+            {description}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ListingCard;
---
+++ src/components/reusableData/listingsDat.ts
@@ -0,0 +1,53 @@
+import landSale from "../../../public/assets/landSale.jpg";
+
+export const listingsData = [
+    {
+      location: 'Kitengela',
+      description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+      image: landSale,
+      id: 1
+    },
+    {
+        location: 'Kitengela',
+        description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+        image: landSale,
+        id: 2
+      },
+      {
+        location: 'Kitengela',
+        description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+        image: landSale,
+        id: 3
+      },
+      {
+        location: 'Kitengela',
+        description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+        image: landSale,
+        id: 4
+      },
+      {
+        location: 'Kitengela',
+        description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+        image: landSale,
+        id: 5
+      },
+      {
+        location: 'Kitengela',
+        description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+        image: landSale,
+        id: 6
+      },
+      {
+        location: 'Kitengela',
+        description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+        image: landSale,
+        id: 7
+      },
+      {
+        location: 'Kitengela',
+        description: 'Stunning and beautiful land with electricity, water, and access to quality road.',
+        image: landSale,
+        id: 8
+      },
+  ];
+  
